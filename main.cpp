f8dfc4fd8040ed7a12686e0fb10b0840ffa687b4
nguyenhpl <258397@edu.p.lodz.pl>
2025-02-10 12:31:18 +0100

    zmiania metody policz

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-
+int policz(int a,int b) {
+    return a*b;
+}
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "w pierwszym branchu; 2+3="<<policz(2,3) << std::endl;
     return 0;
 }
